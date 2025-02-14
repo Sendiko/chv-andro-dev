c14e368cb00fb04f9784b88a90cfa3d9795e1b9d
Sendiko <sarangtawon897@gmail.com>
2025-02-14 21:50:46 +0700

    ref(): change package name

--- app/src/main/java/com/example/belajarandroid/logindemo/LoginScreen.kt
+++ app/src/main/java/com/example/belajarandroid/logindemo/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.example.belajarandroid.login_demo
+package com.example.belajarandroid.logindemo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
